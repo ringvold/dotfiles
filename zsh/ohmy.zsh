bba5badc4bab297054cab509bf903d55c2c24be0
Harald Ringvold <harald.ringvold@vegvesen.no>
2016-07-28 10:51:49 +0200

    Load babun theme when appropriate

--- zsh/ohmy.zsh
+++ zsh/ohmy.zsh
@@ -1,11 +1,12 @@
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
 
-# Set name of the theme to load.
-# Look in ~/.oh-my-zsh/themes/
-# Optionally, if you set this to "random", it'll load a random theme each
-# time that oh-my-zsh is loaded.
-ZSH_THEME="apple"
+# Use Babun the if in Babun (shell gets much faster!)
+if type babun > /dev/null; then
+	ZSH_THEME="babun"
+else
+	ZSH_THEME="apple"
+fi
 
 # Set to this to use case-sensitive completion
 # CASE_SENSITIVE="true"
@@ -39,4 +40,3 @@ DISABLE_CORRECTION="true"
 plugins=(bundler git hub golang brew autojump sublime)
 
 source $ZSH/oh-my-zsh.sh
-source $ZSH_CUSTOM/*.zsh
