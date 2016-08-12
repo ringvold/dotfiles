edb2c699f22dabc2cd3531d5e96ded080f2a7847
Harald Ringvold <harald.ringvold@gmail.com>
2016-08-13 00:06:33 +0200

    Move mac aliases to dofiles-local

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,15 +18,7 @@ alias tree4='tree -L 4'
 
 alias pfind='ps aux | grep'
 
-
-# Mac
-alias gwd='pwd | pbcopy'
-alias zshconfig="open ~/.zshrc"
-alias ohmyzsh="open ~/.oh-my-zsh"
-alias stp="open *.subl*"
-
 alias v=vagrant
-alias railsotto='open "http://$(otto dev address):3000"'
 
 
 function serve {
@@ -35,50 +27,3 @@ function serve {
 }
 
 
-### ENONIC XP
-
-# list the XP homes
-alias xphomes='ls -l ~/code/enonic/xp-homes'
-
-# show the current XP_HOME
-alias xpshowhome='echo $XP_HOME'
-
-# set the XP_HOME variable
-xpsethome() { export XP_HOME=~/code/enonic/xp-homes/"$1"/home ; }
-
-# start a xp server
-xpstart() {
-  VERSION=$1
-  ~/code/enonic/dist/enonic-xp-${VERSION}/bin/server.sh clean
-}
-
-xpget() {
-  VERSION=$1
-  ENONIC_DIST=~/code/enonic/dist
-
-  zparseopts -D -E -A Args -- f
-
-  if [[ -d ${ENONIC_DIST}/enonic-xp-$VERSION ]] && (( ! ${+Args[-f]} )); then
-    echo "Version $VERSION already exsists"
-  else
-    if [[ ! -a ${ENONIC_DIST}/download/distro-${VERSION}.zip ]]; then
-      wget "http://repo.enonic.com/public/com/enonic/xp/distro/$VERSION/distro-$VERSION.zip" \
-        -P ${ENONIC_DIST}/download
-    fi
-
-    if [[ -d ${ENONIC_DIST}/enonic-xp-${VERSION} ]]; then
-      echo "Delete old install"
-      rm -rf ${ENONIC_DIST}/enonic-xp-${VERSION}
-    fi
-    unzip ${ENONIC_DIST}/download/distro-${VERSION}.zip -d ${ENONIC_DIST}
-  fi
-}
-
-# Go to XP 5.3.1 toolbox
-alias xptoolbox531='cd ~/code/enonic/dist/enonic-xp-5.3.1/toolbox'
-
-# Go to XP 6.0.0 toolbox
-alias xptoolbox600='cd ~/code/enonic/dist/enonic-xp-6.0.0/toolbox'
-
-# Go to XP snapshot toolbox
-# alias xptoolbox='cd /Users/mla/dev/sourcecode/xp/modules/distro/target/install/toolbox'
