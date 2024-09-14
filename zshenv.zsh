0bb0dcfca3fdd9e88429aa728b9f6461b1d45c26
Harald Ringvold <harald.ringvold@gmail.com>
2024-09-14 14:08:06 +0200

    Enable history in iex

--- zshenv.zsh
+++ zshenv.zsh
@@ -1,6 +1,7 @@
 export XDG_CONFIG_HOME="$HOME/.config"
 export ZDOTDIR="${HOME}/.config/zsh"
 export ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+export ELIXIR_ERL_OPTIONS="-kernel shell_history enabled"
 
 # Uncomment to activate debug mode
 #export ZSH_DEBUG=false
